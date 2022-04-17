c8ac65c02896e594d8e6b98bc8a60de6e9861bc5
Pablo Baldovi <pbaldovi@gmail.com>
2022-04-17 09:55:28 -0300

    Versión de desarrollo

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,7 +22,7 @@ return new class extends Migration
             $table->rememberToken();
             $table->foreignId('current_team_id')->nullable();
             $table->string('profile_photo_path', 2048)->nullable();
-            $table->string('google_id', 50);
+            $table->string('google_id', 50)->nullable()->default(null);
             $table->timestamps();
         });
     }
